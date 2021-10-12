eb1651a4e43bb9600e1f553d20e67893a40f493e
Tyler Vezina <tvezina@accedian.com>
2021-10-12 14:04:18 -0400

    Added ember-cli-sass and a custom font to play with

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,13 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+const nodeSass = require('node-sass');
+
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      implementation: nodeSass,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
