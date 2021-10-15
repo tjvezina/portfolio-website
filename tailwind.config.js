93d4b09455a3ae2b4d981c884d4a3c53620bcac2
Tyler Vezina <tvezina@accedian.com>
2021-10-14 20:37:31 -0400

    Blocked out basic elements and added a few images

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    borderWidth: {
+      DEFAULT: '1px',
+      0: '0',
+      2: '2px',
+      3: '3px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
+      12: '12px',
+      16: '16px',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
