26f72ed933a9243ddfa1e79a88a9b33ca879861c
Tyler Vezina <tylerjvezina@gmail.com>
2024-05-23 19:49:43 -0400

    Server routing test: added webpack `publicPath`

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
   devtool: 'inline-source-map',
   devServer: {
